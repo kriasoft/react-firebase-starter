98e5a0ef6df8e89f764a8c013d71a8f945f154e3
Konstantin Tarkus <hello@tarkus.me>
2015-09-26 09:11:56 +0300

    Add `debug` prop to Html component

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -14,6 +14,7 @@ class Html extends Component {
     title: PropTypes.string,
     description: PropTypes.string,
     body: PropTypes.string.isRequired,
+    debug: PropTypes.bool.isRequired,
   };
 
   render() {
@@ -27,10 +28,10 @@ class Html extends Component {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
+        <script src={'/app.js?' + new Date().getTime()}></script>
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{__html: this.props.body}} />
-        <script src="/app.js"></script>
         <GoogleAnalytics />
       </body>
       </html>
--- tools/render.js
+++ tools/render.js
@@ -10,12 +10,14 @@ import ReactDOM from 'react-dom/server';
 import Html from '../components/Html';
 import fs from './lib/fs';
 
+const DEBUG = !process.argv.includes('release');
+
 async function render(page, component) {
   const data = {
     body: ReactDOM.renderToString(component),
   };
   const file = join(__dirname, '../build', page.file.substr(0, page.file.lastIndexOf('.')) + '.html');
-  const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
+  const html = ReactDOM.renderToStaticMarkup(<Html debug={DEBUG} {...data} />);
   await fs.mkdir(dirname(file));
   await fs.writeFile(file, html);
 }
