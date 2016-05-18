678efadfb2dc8ca7eb2213674a54235009717cd2
Konstantin Tarkus <hello@tarkus.me>
2016-05-18 18:48:14 +0300

    Update npm modules; add more plugins to PostCSS

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -122,9 +122,21 @@ const config = {
       // W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
       // https://github.com/postcss/postcss-custom-properties
       require('postcss-custom-properties')(),
+      // W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
+      // https://github.com/postcss/postcss-custom-media
+      require('postcss-custom-media')(),
+      // CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
+      // https://github.com/postcss/postcss-media-minmax
+      require('postcss-media-minmax')(),
+      // W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
+      // https://github.com/postcss/postcss-custom-selectors
+      require('postcss-custom-selectors')(),
       // W3C calc() function, e.g. div { height: calc(100px - 2em); }
       // https://github.com/postcss/postcss-calc
       require('postcss-calc')(),
+      // Allows you to nest one style rule inside another
+      // https://github.com/jonathantneal/postcss-nesting
+      require('postcss-nesting')(),
       // W3C color() function, e.g. div { background: color(red alpha(90%)); }
       // https://github.com/postcss/postcss-color-function
       require('postcss-color-function')(),
@@ -134,9 +146,9 @@ const config = {
       // Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
       // https://github.com/robwierzbowski/node-pixrem
       require('pixrem')(),
-      // Pseudo elements such as a:before { }
-      // https://github.com/axa-ch/postcss-pseudoelements
-      require('postcss-pseudoelements')(),
+      // W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
+      // https://github.com/postcss/postcss-selector-matches
+      require('postcss-selector-matches')(),
       // Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
       // https://github.com/postcss/postcss-selector-not
       require('postcss-selector-not')(),
