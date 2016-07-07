eb6542985148b28ae7f9530b3cc9eb8abdc87e69
Konstantin Tarkus <hello@tarkus.me>
2016-07-07 15:47:21 +0300

    Upgrade Webpack to 2.x

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,7 @@ const config = {
       'process.env.NODE_ENV': isDebug ? '"development"' : '"production"',
       __DEV__: isDebug,
     }),
+    new webpack.NamedModulesPlugin(),
     // Emit a JSON file with assets paths
     // https://github.com/sporto/assets-webpack-plugin#options
     new AssetsPlugin({
@@ -187,6 +188,9 @@ const config = {
   },
 
 };
+
+// Integrate Webpack 2.x (replace 'es2015' preset with 'es2015-webpack')
+babelConfig.presets = babelConfig.presets.map(x => (x === 'es2015' ? `${x}-native-modules` : x));
 
 // Optimize the bundle in release (production) mode
 if (!isDebug) {
