ed4e0206c0cdfb1de55b007fcde69262fcd01dd7
Lukas Liesis <liesislukas@gmail.com>
2016-09-09 11:55:35 +0300

    Update store.js
    fix typo for reducers

--- core/store.js
+++ core/store.js
@@ -13,7 +13,7 @@ import { createStore } from 'redux';
 // Centralized application state
 // For more information visit http://redux.js.org/
 const store = createStore((state, action) => {
-  // TODO: Add action handlers (aka "reduces")
+  // TODO: Add action handlers (aka "reducers")
   switch (action) {
     case 'COUNT':
       return { ...state, count: (state.count || 0) + 1 };
