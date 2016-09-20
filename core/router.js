edf4d85b0d9bbdd341b9f1a71473c1088d9a964b
tlimited <tommyvitikka@gmail.com>
2016-09-20 12:52:57 +0200

    const url can't/shouldn't be altered, changed to let

--- core/router.js
+++ core/router.js
@@ -66,7 +66,7 @@ function resolve(routes, context) {
         ...keys.map(key => {
           const query = route.data[key];
           const method = query.substring(0, query.indexOf(' ')); // GET
-          const url = query.substr(query.indexOf(' ') + 1);      // /api/tasks/$id
+          let url = query.substr(query.indexOf(' ') + 1);      // /api/tasks/$id
           // TODO: Optimize
           Object.keys(params).map( (k) =>{
             url = url.replace('$'+k, params[k])
