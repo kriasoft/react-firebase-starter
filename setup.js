94b9e0522a44d9e724b59e1fa4762ca002cf26ef
Konstantin Tarkus <hello@tarkus.me>
2017-08-15 10:02:44 +0300

    Move tests to src/__test__; remove node-fetch dependency (#240)

--- setup.js
+++ setup.js
@@ -7,16 +7,14 @@
 
 const fs = require('fs');
 const path = require('path');
-const fetch = require('node-fetch');
+const https = require('https');
 
 let file, text, search;
 
 //
 // Inject CSS Modules into webpack.config.dev.js
 // -----------------------------------------------------------------------------
-file = path.resolve(
-  './node_modules/react-scripts/config/webpack.config.dev.js',
-);
+file = path.resolve('./node_modules/react-scripts/config/webpack.config.dev.js'); // prettier-ignore
 text = fs.readFileSync(file, 'utf8');
 search = /importLoaders: 1,\n\s{16}}/;
 
@@ -26,8 +24,7 @@ if (text.match(search)) {
     `importLoaders: 1,
                   modules: true,
                   localIdentName: '[name]-[local]-[hash:base64:5]',
-                }`,
-  );
+                }`); // prettier-ignore
   fs.writeFileSync(file, text, 'utf8');
 } else if (!text.indexOf('[name]-[local]-[hash:base64:5]') === -1) {
   throw new Error(`Failed to inject CSS Modules into ${file}`);
@@ -48,8 +45,7 @@ if (text.match(search)) {
     `importLoaders: 1,
                         modules: true,
                         localIdentName: '[hash:base64:5]',
-                        minimize: true,`,
-  );
+                        minimize: true,`); // prettier-ignore
   fs.writeFileSync(file, text, 'utf8');
 } else if (!text.indexOf('[hash:base64:5]') === -1) {
   throw new Error(`Failed to inject CSS Modules into ${file}`);
@@ -65,8 +61,7 @@ if (!text.includes('babel-plugin-relay')) {
   if (text.includes('const plugins = [')) {
     text = text.replace(
       'const plugins = [',
-      "const plugins = [\n  require.resolve('babel-plugin-relay'),",
-    );
+      "const plugins = [\n  require.resolve('babel-plugin-relay'),"); // prettier-ignore
     fs.writeFileSync(file, text, 'utf8');
   } else {
     throw new Error(`Failed to inject babel-plugin-relay in ${file}.`);
@@ -77,9 +72,12 @@ if (!text.includes('babel-plugin-relay')) {
 // Download the GraphQL schema
 // -----------------------------------------------------------------------------
 if (process.argv.includes('--download-schema')) {
-  fetch('https://graphql-demo.kriasoft.com/schema')
-    .then(x => x.text())
-    .then(x =>
-      fs.writeFileSync(path.resolve('./src/schema.graphql'), x, 'utf8'),
-    );
+  file = fs.createWriteStream('./src/schema.graphql');
+  https.get('https://graphql-demo.kriasoft.com/schema', resp => {
+    if (resp.statusCode === 200) {
+      resp.pipe(file);
+    } else {
+      throw new Error('Failed to download the schema.');
+    }
+  });
 }
