f2eb0011ac6f69b2e14cb126109a63bb677dd588
mlesk <lesk.mark.gmail.com>
2017-08-18 17:34:25 -0400

    created inject method and added logic to backup modified react-script files

--- setup.js
+++ setup.js
@@ -9,70 +9,82 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 
-let file, text, search;
+//
+// Create back up file if it is not already present
+// -----------------------------------------------------------------------------
+async function backupIfNotPresent(filePath) {
+  const origFilePath = path.resolve(filePath);
+  const backFilePath = path.resolve(filePath + '.bak');
+  if (!fs.existsSync(backFilePath)) {
+    const stream = fs.createReadStream(origFilePath);
+    stream.pipe(fs.createWriteStream(backFilePath));
+    return new Promise(function(resolve, reject) {
+      stream.on('end', () => resolve(true));
+      stream.on('error', reject); // or something like that
+    });
+  }
+  return new Promise(() => true);
+}
+
+//
+// Inject replacement text over text macthing searchRegex in specified file
+// -----------------------------------------------------------------------------
+async function inject(filePath, searchRegex, replaceText) {
+  await backupIfNotPresent(filePath);
+  const file = path.resolve(filePath);
+  const origText = fs.readFileSync(file, 'utf8');
+  if (origText.indexOf(replaceText) === -1) {
+    if (origText.match(searchRegex)) {
+      const updatedText = origText.replace(searchRegex, replaceText);
+      fs.writeFileSync(file, updatedText, 'utf8');
+    } else {
+      throw new Error(
+        `Failed to inject ${replaceText}
+          for ${searchRegex.toString()}
+          into ${filePath}`,
+      );
+    }
+  }
+}
 
 //
 // Inject CSS Modules into webpack.config.dev.js
 // -----------------------------------------------------------------------------
-file = path.resolve('./node_modules/react-scripts/config/webpack.config.dev.js'); // prettier-ignore
-text = fs.readFileSync(file, 'utf8');
-search = /importLoaders: 1,\n\s{16}}/;
-
-if (text.match(search)) {
-  text = text.replace(
-    search,
-    `importLoaders: 1,
-                  modules: true,
-                  localIdentName: '[name]-[local]-[hash:base64:5]',
-                }`); // prettier-ignore
-  fs.writeFileSync(file, text, 'utf8');
-} else if (!text.indexOf('[name]-[local]-[hash:base64:5]') === -1) {
-  throw new Error(`Failed to inject CSS Modules into ${file}`);
-}
+inject(
+  './node_modules/react-scripts/config/webpack.config.dev.js',
+  /importLoaders: 1,\n\s{16}}/,
+  `importLoaders: 1,
+        modules: true,
+        localIdentName: '[name]-[local]-[hash:base64:5]',
+      }`,
+);
 
 //
 // Inject CSS Modules into webpack.config.prod.js
 // -----------------------------------------------------------------------------
-file = path.resolve(
+inject(
   './node_modules/react-scripts/config/webpack.config.prod.js',
+  /importLoaders: 1,\n\s{24}minimize: true,/,
+  `importLoaders: 1,
+                    modules: true,
+                    localIdentName: '[hash:base64:5]',
+                    minimize: true,`,
 );
-text = fs.readFileSync(file, 'utf8');
-search = /importLoaders: 1,\n\s{24}minimize: true,/;
-
-if (text.match(search)) {
-  text = text.replace(
-    search,
-    `importLoaders: 1,
-                        modules: true,
-                        localIdentName: '[hash:base64:5]',
-                        minimize: true,`); // prettier-ignore
-  fs.writeFileSync(file, text, 'utf8');
-} else if (!text.indexOf('[hash:base64:5]') === -1) {
-  throw new Error(`Failed to inject CSS Modules into ${file}`);
-}
 
 //
 // Inject "babel-plugin-relay"
 // -----------------------------------------------------------------------------
-file = path.resolve('./node_modules/babel-preset-react-app/index.js');
-text = fs.readFileSync(file, 'utf8');
-
-if (!text.includes('babel-plugin-relay')) {
-  if (text.includes('const plugins = [')) {
-    text = text.replace(
-      'const plugins = [',
-      "const plugins = [\n  require.resolve('babel-plugin-relay'),"); // prettier-ignore
-    fs.writeFileSync(file, text, 'utf8');
-  } else {
-    throw new Error(`Failed to inject babel-plugin-relay in ${file}.`);
-  }
-}
+inject(
+  './node_modules/babel-preset-react-app/index.js',
+  /const plugins = \[/,
+  "const plugins = [\n  require.resolve('babel-plugin-relay'),",
+);
 
 //
 // Download the GraphQL schema
 // -----------------------------------------------------------------------------
 if (process.argv.includes('--download-schema')) {
-  file = fs.createWriteStream('./src/schema.graphql');
+  const file = fs.createWriteStream('./src/schema.graphql');
   https.get('https://graphql-demo.kriasoft.com/schema', resp => {
     if (resp.statusCode === 200) {
       resp.pipe(file);
